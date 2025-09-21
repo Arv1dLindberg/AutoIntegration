764c720d07d38b327da8abd64c7f3969c1655a0f
Arvid Lindberg <pl.arvid@gmail.com>
2025-09-21 07:56:59 +0200

    Set up Integration-System. Copied IT-System Models, DbContext and EF config

---
+++ AutoIntegration/Integration-System/Data/ItDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Integration_System.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration_System.Data;
+
+public class ItDbContext : DbContext
+{
+    public ItDbContext(DbContextOptions<ItDbContext> options) : base(options) { }
+
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<ProductionLog> ProductionLogs => Set<ProductionLog>();
+}
